3e8b315b7e637d2f80c332aaa01b12fc56b03917
lejinbo <504056753@qq.com>
2024-12-18 11:19:36 +0800

    first

---
+++ src/test/java/com/example/orderddd/OrderDddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.orderddd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderDddApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
